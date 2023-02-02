6c3068cfffa41450b8fa30365cff1851287c137e
Thomas Maier <contact@thomas-maier.net>
2023-02-02 20:01:41 +0100

    cleanup for new project

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # vaulty
-Web-GUI for Hashicorp's Vault (With LDAP-Auth, used for Multitenant password management)
--- src/routes.js
+++
@@ -1,7 +0,0 @@
-import Login from './components/Login';
-import Secrets from './components/Secrets';
-
-export default {
-  Login: Login,
-  Secrets: Secrets
-};
